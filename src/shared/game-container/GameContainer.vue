95369c964c6dc3093a029a28c04068c4fbf1c7f6
Alex <fornikaras7@gmail.com>
2025-04-22 18:28:18 +0500

    feat: add animations to game container

--- src/shared/game-container/GameContainer.vue
+++ src/shared/game-container/GameContainer.vue
@@ -1,6 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const isVisible = ref(false)
+
+onMounted(() => {
+  requestAnimationFrame(() => {
+    isVisible.value = true
+  })
+})
+</script>
 <template>
-  <div class="container">
+  <div class="container" :class="{ appear: isVisible }">
     <slot name="content"></slot>
   </div>
 </template>
@@ -21,8 +31,17 @@
   z-index: 100;
   justify-content: space-between;
   overflow: hidden;
+  opacity: 0;
+  transform: scale(0);
+  transition:
+    transform 0.7s ease,
+    opacity 0.9s ease;
   @media (max-width: 768px) {
     padding: 20px;
   }
+}
+.container.appear {
+  opacity: 1;
+  transform: scale(1);
 }
 </style>
